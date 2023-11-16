bace91cb7169addfb4af2facb54b02775bee3c29
unclejames2610 <udealaebuka@gmail.com>
2023-11-16 10:15:28 +0100

    chore:refactored the functions in the views.py to be classes

--- books_api/urls.py
+++ books_api/urls.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 from django.urls import path
-from books_api.views import book_list, book_create, book
+# from books_api.views import book_list, book_create, book
+from books_api.views import BookCreate, BookList
 
 urlpatterns = [
-    path('list/', book_list),
-    path('', book_create),
-    path('<int:pk>', book)
+    # path('list/', book_list),
+    # path('', book_create),
+    # path('<int:pk>', book)
+    path('list/', BookList.as_view()),
+    path('', BookCreate.as_view())
 ]
